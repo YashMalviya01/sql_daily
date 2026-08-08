d63604e445c1921f31db7cdf77d2c8295cd78fc1
Yash Malviya <141807402+YashMalviya01@users.noreply.github.com>
2026-08-08 14:39:37 +0530

    Update 8-08-26.sql

--- 8-08-26.sql
+++ 8-08-26.sql
@@ -167,3 +167,117 @@ FROM inactive_customers
 ORDER BY
     customer_id,
     return_date;
+
+/*An e-commerce company wants to identify products whose monthly revenue increased for 3 consecutive months.
+Requirement
+
+For each product, identify the periods where revenue increased month-over-month for three consecutive months.
+
+Return:
+
+product_id
+The month where the 3-month growth streak is completed
+Revenue for that month
+Previous month revenue
+Growth percentage*/
+
+
+WITH monthly_revenue AS
+(
+    SELECT
+        product_id,
+        DATE_TRUNC('month', order_date) AS sales_month,
+        SUM(sales_amount) AS monthly_revenue
+    FROM fact_sales
+    GROUP BY product_id,
+             DATE_TRUNC('month', order_date)    
+),
+
+previous_revenue AS
+(
+    SELECT
+        product_id,
+        sales_month,
+        monthly_revenue,
+
+        LAG(monthly_revenue) OVER (PARTITION BY product_id ORDER BY sales_month) AS previous_month_revenue,
+
+        LAG(sales_month) OVER ( PARTITION BY product_id ORDER BY sales_month) AS monthly_revenue
+),
+
+growth_calculation AS
+(
+    SELECT
+        product_id,
+        sales_month,
+        monthly_revenue,
+        previous_month_revenue,
+
+        ROUND(( monthly_revenue - previous_month_revenue) * 100.0 / NULLIF(previous_month_revenue,0),2) AS growth_oercentage,
+
+        CASE
+            WHEN previous_month_revenue IS NOT NULL
+                 AND monthly_revenue > previous_month_revenue
+            THEN 1
+            ELSE 0
+        END AS growth_flag
+    FROM previous_revenue
+),
+
+growth_streak AS
+(
+    SELECT
+        product_id,
+        sales_month,
+        monthly_revenue,
+        previous_month_revenue,
+        growth_oercentage,
+
+        SUM(growth_flag) AS growth_streak_count
+
+    FROM growth_calculation
+    GROUP BY product_id,
+             sales_month,
+             monthly_revenue,
+             previous_month_revenue,
+             growth_oercentage
+),
+
+max_streak AS 
+(
+    SELECT
+        product_id,
+        MAX(growth_streak_count) AS max_growth_streak
+
+    FROM growth_streak
+    GROUP BY product_id
+),
+
+ranked_products AS 
+(
+    SELECT
+        g.product_id,
+        g.sales_month,
+        g.monthly_revenue,
+        g.previous_month_revenue,
+        g.growth_oercentage,
+        g.growth_streak_count,
+
+        RANK() OVER (PARTITION BY g.product_id ORDER BY g.sales_month DESC) AS rank
+
+    FROM growth_streak g
+    JOIN max_streak m ON g.product_id = m.product_id
+    WHERE g.growth_streak_count = m.max_growth_streak
+)
+
+SELECT
+    product_id,
+    sales_month,
+    monthly_revenue,
+    previous_month_revenue,
+    growth_oercentage
+FROM ranked_products
+WHERE rank <= 10
+ORDER BY product_id,
+         max_growth_streak DESC;
+         
