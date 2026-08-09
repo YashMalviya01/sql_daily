dc9b777324e4b2127002e0578cb40239ba03f976
Yash Malviya <141807402+YashMalviya01@users.noreply.github.com>
2026-08-09 18:27:25 +0530

    Add query for customers with diverse purchases
    Add SQL query to identify customers with purchases from at least 3 categories.

--- 9-08-26.sql
+++ 9-08-26.sql
@@ -1,1 +1,12 @@
+/*An e-commerce company wants to identify customers who have made purchases from at least 3 different product categories.*/
 
+SELECT
+    customer_id,
+    COUNT(DISTINCT p.category) AS category_count,
+    SUM(f.sales_amount) AS total_spent
+FROM fact_sales f
+JOIN dim_products p ON f.product_id = p.product_id
+GROUP BY f.customer_id
+HAVING COUNT(DISTINCT p.category) >= 3
+ORDER BY total_spent DESC
+         category_count DESC;     
